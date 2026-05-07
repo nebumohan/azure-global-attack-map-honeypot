c259a7ce6852e6a8ffe129a736d832ddcac9c3b0
nebumohan <nbmohan6@gmail.com>
2026-05-07 12:40:45 +0530

    Initial commit - Azure Global Attack Map Honeypot

---
+++ scripts/Log_Exporter.ps1
@@ -0,0 +1,13 @@
+<#
+Original Script Author: Josh Madakor
+Original Source:
+https://github.com/joshmadakor1/Sentinel-Lab/blob/main/Custom_Security_Log_Exporter.ps1
+
+This script was adapted and used for educational purposes
+as part of the Azure Global Attack Map Honeypot project.
+
+Modifications:
+- Added custom log formatting
+- Integrated with Azure Monitor workflow
+- Added GeoIP enrichment support
+#>
